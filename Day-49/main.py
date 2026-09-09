d9ca63d64614b53ac73ff4db1c19c7baca659b38
Diwas Regmi <sawid.imger2019@gmail.com>
2026-09-09 21:50:54 +0545

    day -49 so far

--- Day-49/main.py
+++ Day-49/main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common import driver_finder
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+from dotenv import load_dotenv
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+load_dotenv()
+
+EMAIL = os.environ['EMAIL']
+PASSWORD = os.environ['PASSWORD']
+URL = os.environ['URL']
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+# Set path and add profile argument
+user_data_dir = os.path.join(os.getcwd(), "chrome_profile")
+chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL)
+driver.maximize_window()
+
+login_button = driver.find_element(By.ID, value="login-button")
+login_button.click()
+
+email_input = driver.find_element(By.XPATH, value='/html/body/div/main/div/form/div[1]/input')
+email_input.send_keys(EMAIL)
+
+password_input = driver.find_element(By.XPATH, value='/html/body/div/main/div/form/div[2]/input')
+password_input.send_keys(PASSWORD)
+
+submit_button = driver.find_element(By.XPATH,value = '/html/body/div/main/div/form/button')
+submit_button.click()
+
+class_schedule = driver.find_elements(By.XPATH, value="/html/body/div/main/div")
+for classes in c    
+
+# # Wait up to 10 seconds for the element to load
+# wait = WebDriverWait(driver, 10)
+# booked_date = wait.until(
+#     EC.presence_of_element_located((By.ID, "day-title-tue,-sep-15"))
+# )
+# print(booked_date.text)
+#
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# driver.close()
